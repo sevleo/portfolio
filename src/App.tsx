db76219b7fe3ac2bce10d35566b3f9882b978826
Seva <seva.leonov@hotmail.com>
2024-06-24 21:10:38 +0700

    Update tailwindcss setup

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,6 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
-        <p className="bg-red-500">helloooo</p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
