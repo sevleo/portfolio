0121a29eed35cee9021c3e78d2bfe5fb00d2371c
Seva <seva.leonov@hotmail.com>
2024-06-25 14:37:50 +0700

    Add loader animation

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./components/Home";
 import Portfolio from "./components/Portfolio";
 import ErrorPage from "./components/ErrorPage";
+import { useEffect, useState } from "react";
+import { Triangle } from "react-loader-spinner";
 
 function App() {
   const router = createBrowserRouter([
@@ -29,10 +31,45 @@ function App() {
     },
   ]);
 
+  const [loadingState, setLoadingState] = useState("loading-in");
+
+  useEffect(() => {
+    const timer1 = setTimeout(() => {
+      setLoadingState("loading-out");
+    }, 1500);
+
+    const timer2 = setTimeout(() => {
+      setLoadingState("");
+    }, 2200);
+
+    return () => {
+      clearTimeout(timer1);
+      clearTimeout(timer2);
+    };
+  }, []);
+
   return (
     <>
-      <div className="noise"></div>
-      <RouterProvider router={router} />
+      <div
+        className={`loading flex h-full w-full items-center justify-center ${loadingState}`}
+      >
+        <div className="loading__mask bg-yellow-400"></div>
+        <div className="loader">
+          <Triangle
+            visible={true}
+            height="80"
+            width="80"
+            color="#4fa94d"
+            ariaLabel="triangle-loading"
+            wrapperStyle={{}}
+            wrapperClass=""
+          />
+        </div>
+      </div>
+      <div className="relative flex h-full w-full items-center justify-center">
+        <div className="noise"></div>
+        <RouterProvider router={router} />
+      </div>
     </>
   );
 }
