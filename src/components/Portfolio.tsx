a7a397d17969dec4ade9082d49183844f4ed52b8
Seva <seva.leonov@hotmail.com>
2024-06-29 11:07:54 +0700

    Add nueva shop project

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -2,8 +2,11 @@ import TableOfContents from "./TableOfContents";
 import { useEffect, useState, useRef, useMemo } from "react";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
 import { createRef } from "react";
-import yogato from "../assets/yogato.png";
 import Project from "./Project";
+
+// Images of projects
+import yogato from "../assets/yogato.png";
+import nuevashop from "../assets/nuevashop.png";
 
 export default function Portfolio({ loadingState }) {
   const [activeId, setActiveId] = useState(undefined);
@@ -683,7 +686,13 @@ export default function Portfolio({ loadingState }) {
               and following through their sequences. It features voice
               announcements, custom-made sequence timer, a smooth and fast user
               experience, and support for both desktop and mobile."
-                ></Project>
+                />
+                <Project
+                  link="https://heroic-horse-859bac.netlify.app/"
+                  imgSrc={nuevashop}
+                  projectName="Nueva Shop"
+                  projectDescription="A minimalist shopping cart implementation with ReactJS. Fetching products data from MockShop API and storing it locally in the browser. Using React components with states to implement neat and smooth user experience."
+                />
               </ul>
             </section>
           </CSSTransition>
